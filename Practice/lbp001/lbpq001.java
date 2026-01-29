c238e651f67f921c9485a5110e1949c08a38ee99
Chandra-Shekhar-24 <chandrashekharbansal.2006@gmail.com>
2026-01-30 01:04:05 +0530

    Project cleanup - removed unused files

--- Practice/lbp001/lbpq001.java
+++ Practice/lbp001/lbpq001.java
@@ -1,42 +1,37 @@
-package Practice.lbp001;
+package practice.lbp001;
 
 import java.util.Scanner;
-
 public class lbpq001 {
-
     public static boolean isPrimeDigit(int number) {
-        if (number == 2 || number == 3 || number == 5 || number == 7) {
+        if(number == 2 || number == 3 || number == 5 || number == 7) {
             return true;
         }
         return false;
-
     }
-
     public static int calculateSumOfPrimeDigit(int number) {
-        int sum = 0;
         int temp = number;
-        while (number > 0) {
-            int lastdigit = number % 10;
-            if (isPrimeDigit(lastdigit)) {
-                sum += lastdigit;
+        int sum = 0;
+        while(temp != 0) {
+            int lastDigit = temp % 10;
+            if(isPrimeDigit(lastDigit)) {
+                sum += lastDigit;
             }
-            number /= 10;
+            temp /= 10;
         }
         return sum;
     }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter number: ");
-
-        if (!scanner.hasNextInt()) {
-            System.out.print("Invalid input");
+        if(!scanner.hasNextInt()) {
+            System.out.println("Invalid input. Please enter only integer value.");
             scanner.close();
             return;
         }
         int number = scanner.nextInt();
 
         int result = calculateSumOfPrimeDigit(number);
-        System.out.println(result);
+        System.out.println("The sum of prime Digit: " + result);
+        scanner.close();
     }
 }
